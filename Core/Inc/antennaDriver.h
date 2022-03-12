e09924bb55c44afcfb231cd1e9f544b6017c6bb8
Etienne Lariviere <etiennasse@hotmail.com>
2022-03-12 12:02:12 -0500

    Antenna driver started
    File creation and branch name changed

---
+++ Core/Inc/antennaDriver.h
@@ -0,0 +1,25 @@
+/*!
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @file antennaDriver.h
+ * @brief Implementation of common commands
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author            Etienne Lariviere
+ * @version           1.0.0
+ * @date              2022.03.12
+ *
+ * @copyright         (C) Copyright Endurosat
+ *
+ *                    Contents and presentations are protected world-wide.
+ *                    Any kind of using, copying etc. is prohibited without prior permission.
+ *                    All rights - incl. industrial property rights - are reserved.
+ *
+ * @history
+ * @revision{         1.0.0  , 2022.03.12, author Etienne Lariviere, Initial revision }
+ * @endhistory
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+#ifndef INC_ANTENNADRIVER_H_
+#define INC_ANTENNADRIVER_H_
+
+#include <stdint.h>
+#include <stdbool.h>
---
+++ Core/Src/antennaDriver.c
@@ -0,0 +1,23 @@
+/*!
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @file antennaDriver.c
+ * @brief Implementation of common commands
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author            Etienne Lariviere
+ * @version           1.0.0
+ * @date              2022.03.12
+ *
+ * @copyright         (C) Copyright Endurosat
+ *
+ *                    Contents and presentations are protected world-wide.
+ *                    Any kind of using, copying etc. is prohibited without prior permission.
+ *                    All rights - incl. industrial property rights - are reserved.
+ *
+ * @history
+ * @revision{         1.0.0  , 2022.03.12, author Etienne Lariviere, Initial revision }
+ * @endhistory
+ *~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+#include "antennaDriver.h"
+#include "i2c.h"
